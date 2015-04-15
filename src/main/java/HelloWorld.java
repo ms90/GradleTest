5d2bedddc39f6e53e2c933f3033f7957673bcc32
Maxim <maxim@134.155.148.61>
2015-04-15 22:29:58 +0200

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @author zen0x
+ * @since 15.04.2015
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        run();
+    }
+
+    private static void run() {
+        System.out.println("Hello World!");
+    }
+}
